e0b321d42faaf84461bb774af4f1b119f12a1f87
xiao-e-yun <a0970323059@gmail.com>
2025-11-22 17:56:42 +0800

    fix: file meta for search

--- src/api/category/file_meta.rs
+++ src/api/category/file_meta.rs
@@ -10,6 +10,7 @@ impl RequireRelations for FileMeta {}
 impl Category for FileMeta {
     type Id = FileMetaId;
     const TABLE_NAME: &'static str = "file_metas";
+    const SEARCH_NAME: &'static str = "filename";
 
     fn from_row(row: &Row) -> Result<Self, rusqlite::Error> {
         FileMeta::from_row(row)
